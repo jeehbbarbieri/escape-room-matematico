90e80fb1934f19ab886cec89e1f6fca051446f58
jeehbbarbieri <jessikah.berns@gmail.com>
2026-09-22 15:20:55 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,24 @@
+const jogo = {
+    titulo: "Minha Sala de Escape",
+
+    desafios: [
+        {
+            titulo: "Sala 1",
+            pergunta: "Edite esta pergunta para criar seu próprio desafio.",
+            resposta: "resposta",
+            dica: "Edite também esta dica no editor."
+        },
+        {
+            titulo: "Sala 2",
+            pergunta: "Você pode criar desafios sobre qualquer conteúdo matemático.",
+            resposta: "resposta",
+            dica: "Use o editor para personalizar esta sala."
+        },
+        {
+            titulo: "Sala 3",
+            pergunta: "Esta é a última sala do jogo-modelo.",
+            resposta: "resposta",
+            dica: "Você também pode adicionar ou excluir salas."
+        }
+    ]
+};
